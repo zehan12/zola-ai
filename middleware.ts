38c7391a957ab5db8e11c0177b7e6a642bd7f2b5
ibelick <julien.thibeaut@gmail.com>
2025-04-06 21:52:11 +0200

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,26 @@
 import { updateSession } from "@/utils/supabase/middleware"
-import { type NextRequest } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const response = await updateSession(request)
+
+  const isDev = process.env.NODE_ENV === "development"
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
+  const supabaseDomain = new URL(supabaseUrl).origin
+
+  response.headers.set(
+    "Content-Security-Policy",
+    isDev
+      ? `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:; connect-src 'self' wss: https://api.openai.com https://api.mistral.ai https://api.supabase.com ${supabaseDomain};`
+      : `default-src 'self'; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://analytics.umami.is https://vercel.live; frame-src 'self' https://vercel.live; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:; connect-src 'self' wss: https://api.openai.com https://api.mistral.ai https://api.supabase.com ${supabaseDomain} https://api-gateway.umami.dev;`
+  )
+
+  return response
 }
 
 export const config = {
   matcher: [
-    // Exclude static files and API routes
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
