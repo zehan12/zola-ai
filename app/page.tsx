ecaee3ec61776933c022d20104b05c9cab0af29a
ibelick <julien.thibeaut@gmail.com>
2025-04-09 22:13:18 +0200

    feat: add force-dynamic in home

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { MessagesProvider } from "@/lib/chat-store/messages/provider"
 import Chat from "./components/chat/chat"
 import LayoutApp from "./components/layout/layout-app"
 
+export const dynamic = "force-dynamic"
+
 export default function Home() {
   return (
     <MessagesProvider>
