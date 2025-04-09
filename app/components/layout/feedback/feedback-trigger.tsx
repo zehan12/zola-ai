bfae60f7112dd7ed82f9300424732824205c48b2
ibelick <julien.thibeaut@gmail.com>
2025-04-09 09:45:21 +0200

    refactor: move app-info

--- app/components/layout/feedback/feedback-trigger.tsx
+++ app/components/layout/feedback/feedback-trigger.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useBreakpoint } from "@/app/hooks/use-breakpoint"
+import { useUser } from "@/app/providers/user-provider"
 import { FeedbackForm } from "@/components/common/feedback-form"
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
@@ -8,11 +9,8 @@ import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { Question } from "@phosphor-icons/react"
 import { useState } from "react"
 
-type FeedbackTriggerProps = {
-  authUserId?: string
-}
-
-export function FeedbackTrigger({ authUserId }: FeedbackTriggerProps) {
+export function FeedbackTrigger() {
+  const { user } = useUser()
   const isMobile = useBreakpoint(768)
   const [isOpen, setIsOpen] = useState(false)
 
@@ -33,7 +31,7 @@ export function FeedbackTrigger({ authUserId }: FeedbackTriggerProps) {
         <Drawer open={isOpen} onOpenChange={setIsOpen}>
           <DrawerTrigger asChild>{trigger}</DrawerTrigger>
           <DrawerContent className="bg-background border-border">
-            <FeedbackForm authUserId={authUserId} onClose={handleClose} />
+            <FeedbackForm authUserId={user?.id} onClose={handleClose} />
           </DrawerContent>
         </Drawer>
       </>
@@ -45,7 +43,7 @@ export function FeedbackTrigger({ authUserId }: FeedbackTriggerProps) {
       <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <DialogTrigger asChild>{trigger}</DialogTrigger>
         <DialogContent className="[&>button:last-child]:bg-background overflow-hidden p-0 shadow-xs sm:max-w-md [&>button:last-child]:top-3.5 [&>button:last-child]:right-3 [&>button:last-child]:rounded-full [&>button:last-child]:p-1">
-          <FeedbackForm authUserId={authUserId} onClose={handleClose} />
+          <FeedbackForm authUserId={user?.id} onClose={handleClose} />
         </DialogContent>
       </Dialog>
     </>
